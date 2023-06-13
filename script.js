98d8c1e5bf7ce3025022c650512b5d2aab1b9206
FraBif <francescobif@hotmail.it>
2023-06-13 16:08:36 +0200

    done

--- script.js
+++ script.js
@@ -3,11 +3,19 @@ const {createApp} = Vue
 createApp ({
     data () {
         return{
-
+           generatedMailsList: []
         }
     },
 
-    methods: {
-        
+    created () {
+        for (let i = 0 ; i < 10 ; i++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(
+                (result) => {
+                    const generatedMail = result.data.response
+                    this.generatedMailsList.push(generatedMail)
+                   
+                }
+            )
+        }
     }
 }).mount ("#app")
